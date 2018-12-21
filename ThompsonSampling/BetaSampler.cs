a1a11ab92ffdc00444a6a4ce90861dbf952f5175
bartczernicki <bartczernicki@gmail.com>
2018-12-20 22:56:42 -0500

    Initial check-in of module ThompsonSampling

---
+++ ThompsonSampling/BetaSampler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ThompsonSamplingDemo
+{
+    public class BetaSampler
+    {
+        public Random rnd;
+        public BetaSampler(int seed)
+        {
+            this.rnd = new Random(seed);
+        }
+
+        public double Sample(double a, double b)
+        {
+            double alpha = a + b;
+            double beta = 0.0;
+            double u1, u2, w, v = 0.0;
+            if (Math.Min(a, b) <= 1.0)
+                beta = Math.Max(1 / a, 1 / b);
+            else
+                beta = Math.Sqrt((alpha - 2.0) /
+          (2 * a * b - alpha));
+            double gamma = a + 1 / beta;
+            while (true)
+            {
+                u1 = this.rnd.NextDouble();
+                u2 = this.rnd.NextDouble();
+                v = beta * Math.Log(u1 / (1 - u1));
+                w = a * Math.Exp(v);
+                double tmp = Math.Log(alpha / (b + w));
+                if (alpha * tmp + (gamma * v) - 1.3862944 >=
+           Math.Log(u1 * u1 * u2))
+                    break;
+            }
+            double x = w / (b + w);
+            return x;
+        }
+    }
+}
---
+++ ThompsonSampling/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ThompsonSamplingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Based on MSDN article: https://msdn.microsoft.com/en-us/magazine/mt829274.aspx
+            Console.WriteLine("Begin Thompson sampling demo");
+
+            int N = 3;
+            double[] means = new double[] { 0.2, 0.25, 0.35 };
+            double[] probs = new double[N];
+            int[] S = new int[N];
+            int[] F = new int[N];
+
+            Random rnd = new Random(4);
+            var rndInt = rnd.Next();
+
+            BetaSampler bs = new BetaSampler(rndInt);
+
+            for (int trial = 0; trial < 25000; ++trial)
+            {
+                Console.WriteLine("Trial " + trial);
+                for (int i = 0; i < N; ++i)
+                    probs[i] = bs.Sample(S[i] + 1.0, F[i] + 1.0);
+
+                Console.Write("sampling probs = ");
+                for (int i = 0; i < N; ++i)
+                    Console.Write(probs[i].ToString("F4") + " ");
+
+                Console.WriteLine(string.Empty);
+                int machine = 0;
+                double highProb = 0.0;
+                for (int i = 0; i < N; ++i)
+                {
+                    if (probs[i] > highProb)
+                    {
+                        highProb = probs[i];
+                        machine = i;
+                    }
+                }
+
+                Console.Write("Playing machine " + machine);
+                double p = rnd.NextDouble();
+
+                if (p < means[machine])
+                {
+                    Console.WriteLine(" -- win");
+                    ++S[machine];
+                }
+                else
+                {
+                    Console.WriteLine(" -- lose");
+                    ++F[machine];
+                }
+            } // End of Trials Loop
+
+            Console.WriteLine("Final estimates of means: ");
+            for (int i = 0; i < N; ++i)
+            {
+                double u = (S[i] * 1.0) / (S[i] + F[i]);
+                Console.WriteLine(u.ToString("F4") + "  ");
+            }
+
+            Console.WriteLine("Number times machine played:");
+            for (int i = 0; i < N; ++i)
+            {
+                int ct = S[i] + F[i];
+                Console.WriteLine(ct + "  ");
+            }
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("End of demo ");
+            Console.ReadLine();
+        }
+    }
+}
